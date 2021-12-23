9fa232b63f18f8c5789e968299121bfb3cc96310
Dominik Jurkowski <dominik.jurkowski@noaignite.com>
2021-12-23 12:18:02 +0100

    Add playlist state and playlist component into sidebar

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -4,84 +4,75 @@ import {
   LibraryIcon,
   PlusCircleIcon,
   RssIcon,
-  HeartIcon
-} from '@heroicons/react/outline';
-import { useSession, signOut } from 'next-auth/react';
+  HeartIcon,
+} from "@heroicons/react/outline";
+import { useSession, signOut } from "next-auth/react";
+import { useState, useEffect } from "react";
+import { useRecoilState } from "recoil";
+import { activePlaylistState } from "../../../atoms/playlistAtom";
+import useSpotify from "../../../hooks/useSpotify";
+import Playlists from "../playlists";
 
 const Sidebar = () => {
-  const {data: session, status} = useSession();
+  const spotifyApi = useSpotify();
+  const { data: session, status } = useSession();
+  const [playlists, setPlaylists] = useState<
+    SpotifyApi.PlaylistObjectSimplified[]
+  >([]);
+  const [activePlaylist, setActivePlaylist] = useRecoilState(activePlaylistState);
+
+  useEffect(() => {
+    if (spotifyApi.getAccessToken()) {
+      spotifyApi.getUserPlaylists().then((data) => {
+        setPlaylists(data.body.items);
+      });
+    }
+  }, [session, spotifyApi]);
+
+  console.log("Your active playlist ", activePlaylist);
+
   return (
-    <div className='text-gray-500 p-5 text-sm border-r border-gray-900'>
-      <div className='space-y-4'>
-      <button 
-        className='flex items-center space-x-2 hover:text-white'
-        onClick={() => signOut()}>
-          <HomeIcon className="h-5 w-5"/>
+    <div className="text-gray-500 p-5 text-sm border-r border-gray-900 overflow-scroll scrollbar-hide h-screen">
+      <div className="space-y-4">
+        <button
+          className="flex items-center space-x-2 hover:text-white"
+          onClick={() => signOut()}
+        >
+          <HomeIcon className="h-5 w-5" />
           <p>Log out</p>
         </button>
-        <button className='flex items-center space-x-2 hover:text-white'>
-          <HomeIcon className="h-5 w-5"/>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <HomeIcon className="h-5 w-5" />
           <p>Home</p>
         </button>
-        <button className='flex items-center space-x-2 hover:text-white'>
-          <SearchIcon className="h-5 w-5"/>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <SearchIcon className="h-5 w-5" />
           <p>Search</p>
         </button>
-        <button className='flex items-center space-x-2 hover:text-white'>
-          <LibraryIcon className="h-5 w-5"/>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <LibraryIcon className="h-5 w-5" />
           <p>Your Library</p>
         </button>
-        <hr className='border-t-[0.1px] border-gray-900'/>
+        <hr className="border-t-[0.1px] border-gray-900" />
 
-        <button className='flex items-center space-x-2 hover:text-white'>
-          <PlusCircleIcon className="h-5 w-5"/>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <PlusCircleIcon className="h-5 w-5" />
           <p>Create Playlist</p>
         </button>
-        <button className='flex items-center space-x-2 hover:text-white'>
-          <HeartIcon className="h-5 w-5"/>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <HeartIcon className="h-5 w-5" />
           <p>Like songs</p>
         </button>
-        <button className='flex items-center space-x-2 hover:text-white'>
-          <RssIcon className="h-5 w-5"/>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <RssIcon className="h-5 w-5" />
           <p>Your episodes</p>
         </button>
-        <hr className='border-t-[0.1px] border-gray-900'/>
+        <hr className="border-t-[0.1px] border-gray-900" />
 
-        <p className='cursor-pointer hover:text-white'>
-          Playlistname...
-        </p>
-        <p className='cursor-pointer hover:text-white'>
-          Playlistname...
-        </p>
-        <p className='cursor-pointer hover:text-white'>
-          Playlistname...
-        </p>
-        <p className='cursor-pointer hover:text-white'>
-          Playlistname...
-        </p>
-        <p className='cursor-pointer hover:text-white'>
-          Playlistname...
-        </p>
-        <p className='cursor-pointer hover:text-white'>
-          Playlistname...
-        </p>
-        <p className='cursor-pointer hover:text-white'>
-          Playlistname...
-        </p>
-        <p className='cursor-pointer hover:text-white'>
-          Playlistname...
-        </p>
-        <p className='cursor-pointer hover:text-white'>
-          Playlistname...
-        </p>
-        <p className='cursor-pointer hover:text-white'>
-          Playlistname...
-        </p>
-        
+        <Playlists playlists={playlists} clickFn={setActivePlaylist}/>
       </div>
     </div>
-  )
+  );
 };
-
 
 export default Sidebar;
