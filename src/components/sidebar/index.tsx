285b07adb5967c4d2d4a3fb1b50d3466516bc9c8
Dominik Jurkowski <dominik.jurkowski@noaignite.com>
2021-12-23 18:45:46 +0100

    Add custome button component

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -12,6 +12,7 @@ import { useRecoilState } from "recoil";
 import { activePlaylistState } from "../../../atoms/playlistAtom";
 import useSpotify from "../../../hooks/useSpotify";
 import Playlists from "../playlists";
+import Button from "./Button";
 
 const Sidebar = () => {
   const spotifyApi = useSpotify();
@@ -29,44 +30,19 @@ const Sidebar = () => {
     }
   }, [session, spotifyApi]);
 
-  console.log("Your active playlist ", activePlaylist);
-
   return (
-    <div className="text-gray-500 p-5 text-sm border-r border-gray-900 overflow-scroll scrollbar-hide h-screen">
+    <div className="text-gray-500 p-5 text-xs lg:text-sm border-r border-gray-900 overflow-scroll scrollbar-hide h-screen sm:max-w-[12rem] lg:max-w-[15rem] hidden md:inline-flex">
       <div className="space-y-4">
-        <button
-          className="flex items-center space-x-2 hover:text-white"
-          onClick={() => signOut()}
-        >
-          <HomeIcon className="h-5 w-5" />
-          <p>Log out</p>
-        </button>
-        <button className="flex items-center space-x-2 hover:text-white">
-          <HomeIcon className="h-5 w-5" />
-          <p>Home</p>
-        </button>
-        <button className="flex items-center space-x-2 hover:text-white">
-          <SearchIcon className="h-5 w-5" />
-          <p>Search</p>
-        </button>
-        <button className="flex items-center space-x-2 hover:text-white">
-          <LibraryIcon className="h-5 w-5" />
-          <p>Your Library</p>
-        </button>
+        {/* <Button text="Log out" Icon={HomeIcon} onClick={() => signOut()}/> */}
+        <Button text="Home" Icon={HomeIcon} />
+        <Button text="Search" Icon={SearchIcon} />
+        <Button text="Your Library" Icon={LibraryIcon} />
         <hr className="border-t-[0.1px] border-gray-900" />
 
-        <button className="flex items-center space-x-2 hover:text-white">
-          <PlusCircleIcon className="h-5 w-5" />
-          <p>Create Playlist</p>
-        </button>
-        <button className="flex items-center space-x-2 hover:text-white">
-          <HeartIcon className="h-5 w-5" />
-          <p>Like songs</p>
-        </button>
-        <button className="flex items-center space-x-2 hover:text-white">
-          <RssIcon className="h-5 w-5" />
-          <p>Your episodes</p>
-        </button>
+        <Button text="Create Playlist" Icon={PlusCircleIcon} />
+        <Button text="Like songs" Icon={HeartIcon} />
+        <Button text="Your episodes" Icon={RssIcon} />
+
         <hr className="border-t-[0.1px] border-gray-900" />
 
         <Playlists playlists={playlists} clickFn={setActivePlaylist}/>
