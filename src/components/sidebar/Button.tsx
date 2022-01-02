8a1e36bbbbbfd3b591ef854bbace0e57a0f476cc
Dominik Jurkowski <dominik.jurkowski@noaignite.com>
2022-01-02 22:54:57 +0100

    Add props responsible for style classes

--- src/components/sidebar/Button.tsx
+++ src/components/sidebar/Button.tsx
@@ -3,13 +3,14 @@ import React from 'react';
 interface ButtonProps {
   text: string,
   Icon: (props: React.ComponentProps<'svg'>) => JSX.Element,
-  onClick?:()=>void
+  onClick?:()=>void,
+  classIconString?:string | undefined,
 };
 
-const Button = ({text, Icon, onClick }: ButtonProps) => {
+const Button = ({text, Icon, onClick, classIconString}: ButtonProps) => {
   return (
-    <button className="flex items-center space-x-2 hover:text-white" onClick={onClick}>
-      <Icon className="h-5 w-5"/>
+    <button className={`flex items-center space-x-2 hover:text-white`} onClick={onClick}>
+      <Icon className={`h-5 w-5 ${classIconString}`}/>
       <p>{text}</p>
     </button>
   )
